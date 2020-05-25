0f009feab08be95d94b55e2119b354b825cdd952
all02 <all02@DESKTOP-KTVLEOE>
2020-05-25 11:24:26 +0900

    DTO 생성

---
+++ maius11/src/main/java/com/kh/entity/boardDto.java
@@ -0,0 +1,12 @@
+package com.kh.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @AllArgsConstructor @NoArgsConstructor @Builder
+public class boardDto {
+	private int board_no, user_no, board_replycount, board_readcount;
+	private String  board_title, board_wdate, board_content;
+}
